e62d9944dbb8a84435d126c05849ca22757c8f2b
Sumit Saraf <ssaraf@liveperson.com>
2024-07-07 22:01:32 +0530

    second commit

--- index.js
+++ index.js
@@ -7,4 +7,38 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Server is listening at ", PORT);
 })
+app.use(express.json());
 
+app.get("/getname", (request, response) => { //https://example.com/api/getname
+    response.status(201).json({
+        statusCode: 1,
+        statusMessage: "SUCCESS",
+        data: {
+            "name" : "Sumit"
+        }
+        
+    })
+})
+
+app.post("/updatename", (req, res) => {
+    try {
+        const lastName = req.body.lastname;
+        console.log(lastName);
+        if (!lastName) throw new Error("Last name does not found");
+
+        res.status(200).json({
+					statusCode: 1,
+					statusMessage: "SUCCESS",
+					data: {
+						name: "Sumit "+lastName,
+					},
+				});
+        
+    } catch (error) {
+        res.status(400).json({
+            statusCode: 0,
+            statusMessage: "ERROR",
+            ErrorMessage : error.message
+        })
+    }
+})
